812c90a511b5b6a55a69104c32642ab974ca1b25
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-05-19 18:59:05 -0400

    added title and close button in the popup

--- src/main/java/re/vianneyfaiv/persephone/ui/fragment/PropertiesPopup.java
+++ src/main/java/re/vianneyfaiv/persephone/ui/fragment/PropertiesPopup.java
@@ -2,8 +2,10 @@ package re.vianneyfaiv.persephone.ui.fragment;
 
 import java.util.Map;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
-import com.vaadin.ui.Panel;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.PopupView;
 import com.vaadin.ui.VerticalLayout;
 
@@ -11,14 +13,24 @@ public class PropertiesPopup extends VerticalLayout {
 
 	public PropertiesPopup(String title, Map<String, String> properties) {
 
-		Grid<PropertyItem> grid = new Grid<>(PropertyItem.class);
+		// Popup title and close button
+		Label popupTitle = new Label(title);
+		Button closeButton = new Button("Close");
+		HorizontalLayout titleBar = new HorizontalLayout(popupTitle, closeButton);
 
+		// Popup grid
+		Grid<PropertyItem> grid = new Grid<>(PropertyItem.class);
 		grid.setColumns("key", "value");
 		grid.setItems(properties.entrySet().stream().map(e -> new PropertyItem(e.getKey(), e.getValue())));
 
-		Panel popupContent = new Panel(grid);
+		// Create Popup
+		VerticalLayout popupContent = new VerticalLayout(titleBar, grid);
 
 		PopupView popup = new PopupView(title, popupContent);
+		popup.setCaption(title);
+		popup.setHideOnMouseOut(false);
+
+		closeButton.addListener(e -> popup.setPopupVisible(false));
 
 		this.addComponent(popup);
 	}
