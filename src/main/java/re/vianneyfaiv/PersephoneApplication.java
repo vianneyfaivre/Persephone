fdf2594fc71d9ec83e57a780cd5fa8f4c196c06f
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-09-25 21:11:51 -0400

    removed datasource autoconfig

--- src/main/java/re/vianneyfaiv/PersephoneApplication.java
+++ src/main/java/re/vianneyfaiv/PersephoneApplication.java
@@ -2,8 +2,9 @@ package re.vianneyfaiv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class PersephoneApplication {
 
 	public static void main(String[] args) {
