5d9470dd10787ab6e0d5aaca4de051303e85717e
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-09-28 22:51:24 -0400

    added persephone related properties file

--- src/main/java/re/vianneyfaiv/PersephoneApplication.java
+++ src/main/java/re/vianneyfaiv/PersephoneApplication.java
@@ -3,8 +3,10 @@ package re.vianneyfaiv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@PropertySource("persephone.properties")
 public class PersephoneApplication {
 
 	public static void main(String[] args) {
