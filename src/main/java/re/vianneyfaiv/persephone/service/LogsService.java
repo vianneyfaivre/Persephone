2fc3c81ad67c6adb1ce36753e1d8f10e6abafc72
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-09-28 21:19:23 -0400

    handle exception when /logfile is not available

--- src/main/java/re/vianneyfaiv/persephone/service/LogsService.java
+++ src/main/java/re/vianneyfaiv/persephone/service/LogsService.java
@@ -2,9 +2,11 @@ package re.vianneyfaiv.persephone.service;
 
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import re.vianneyfaiv.persephone.domain.Application;
+import re.vianneyfaiv.persephone.exception.PersephoneServiceException;
 
 /**
  * Calls /logfile
@@ -15,12 +17,23 @@ public class LogsService {
 	private RestTemplate restTemplate = new RestTemplate();
 
 	public String getLogs(Application app) {
+
 		String url = String.format("%s/%s", app.getUrl(), "logfile");
-		return this.restTemplate.getForObject(url, String.class);
+
+		try {
+			return this.restTemplate.getForObject(url, String.class);
+		} catch(RestClientException e) {
+			throw new PersephoneServiceException(app, e.getMessage());
+		}
 	}
 
 	public ByteArrayResource downloadLogs(Application app) {
 		String url = String.format("%s/%s", app.getUrl(), "logfile");
-		return this.restTemplate.getForObject(url, ByteArrayResource.class);
+
+		try {
+			return this.restTemplate.getForObject(url, ByteArrayResource.class);
+		} catch(RestClientException e) {
+			throw new PersephoneServiceException(app, e.getMessage());
+		}
 	}
 }
--- src/main/java/re/vianneyfaiv/persephone/ui/page/ApplicationLogsPage.java
+++ src/main/java/re/vianneyfaiv/persephone/ui/page/ApplicationLogsPage.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.navigator.View;
@@ -29,6 +31,8 @@ import re.vianneyfaiv.persephone.ui.PersephoneViews;
 @SpringView(name=PersephoneViews.LOGS)
 public class ApplicationLogsPage extends VerticalLayout implements View {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationLogsPage.class);
+
 	@Autowired
 	private ApplicationService appService;
 
@@ -42,8 +46,13 @@ public class ApplicationLogsPage extends VerticalLayout implements View {
 	@Override
 	public void enter(ViewChangeEvent event) {
 
+		this.removeAllComponents();
+
 		// Get application
 		int appId = Integer.valueOf(event.getParameters());
+
+		LOGGER.debug("Loading logs of application with id {}", appId);
+
 		Optional<Application> app = appService.findById(appId);
 		if(!app.isPresent()) {
 			// TODO: throw exception
@@ -53,25 +62,31 @@ public class ApplicationLogsPage extends VerticalLayout implements View {
 		String title = String.format("<h2>%s (%s): Logs</h2>", app.get().getName(), app.get().getEnvironment());
 		this.addComponent(new Label(title, ContentMode.HTML));
 
-		// Download logs button ByteArrayResource logsBytes = logsService.downloadLogs(app.get());
+		// Download logs button
 		Button downloadButton = new Button("Download full logfile");
 		this.addComponent(downloadButton);
 
-		try (InputStream logsStream = logsService.downloadLogs(app.get()).getInputStream()) {
-			StreamResource resource = new StreamResource(() -> logsStream, "logs.txt");
+		// Download logs button : on click
+		downloadButton.addClickListener(e -> {
+			StreamResource resource = new StreamResource(() -> {
+				try (InputStream logsStream = logsService.downloadLogs(app.get()).getInputStream()) {
+					return logsStream;
+				} catch (IOException ex) {
+					throw new PersephoneServiceException(app.get(), String.format("Unable to get logs file: %s", ex.getMessage()));
+				}
+			}, "logs.txt");
 
 			FileDownloader fileDownloader = new FileDownloader(resource);
 			fileDownloader.setOverrideContentType(false);
 			fileDownloader.extend(downloadButton);
-		} catch (IOException e) {
-			throw new PersephoneServiceException(app.get(), String.format("Unable to get logs file: %s", e.getMessage()));
-		}
+		});
 
 		// Back button
 		this.addComponent(new Button("Back to applications list", e -> getUI().getNavigator().navigateTo(PersephoneViews.APPLICATIONS)));
 
 		// Get logs
 		String logs = logsService.getLogs(app.get());
+		System.out.println("logs : "+logs.length());
 		this.addComponent(new Label(logs, ContentMode.PREFORMATTED));
 	}
 
