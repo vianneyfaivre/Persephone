374c04de6d5d6270d2209f0b876c7d7f0c75d68e
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-10-03 22:46:37 -0400

    ability to add Component instead of Button

--- src/main/java/re/vianneyfaiv/persephone/ui/component/ButtonBar.java
+++ src/main/java/re/vianneyfaiv/persephone/ui/component/ButtonBar.java
@@ -1,20 +1,21 @@
 package re.vianneyfaiv.persephone.ui.component;
 
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 
 import re.vianneyfaiv.persephone.ui.PersephoneViews;
 
 /**
- * Horizontal button bar that have at least a back to applications button
+ * Horizontal components bar that have at least a "back to applications" button
  */
 public class ButtonBar extends HorizontalLayout {
 
-	public ButtonBar(Button... buttons) {
+	public ButtonBar(Component... components) {
 		Button backButton = new Button("Back to applications list",
 											e -> getUI().getNavigator().navigateTo(PersephoneViews.APPLICATIONS));
 
-		this.addComponents(buttons);
+		this.addComponents(components);
 		this.addComponent(backButton);
 	}
 }
