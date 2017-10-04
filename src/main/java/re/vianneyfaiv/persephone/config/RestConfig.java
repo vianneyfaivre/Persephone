33e249c26e6e03adea1d8db81f40d47278800709
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-10-03 20:03:29 -0400

    set default values for HTTP requests timeout

--- src/main/java/re/vianneyfaiv/persephone/config/RestConfig.java
+++ src/main/java/re/vianneyfaiv/persephone/config/RestConfig.java
@@ -1,15 +1,41 @@
 package re.vianneyfaiv.persephone.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
 
+	@Value("${persephone.rest-template.read-timeout-seconds:}")
+	private String readTimeout;
+
+	@Value("${persephone.rest-template.connect-timeout-seconds:}")
+	private String connectTimeout;
+
 	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplate rt = new RestTemplate();
+	public ClientHttpRequestFactory requestFactory() {
+		SimpleClientHttpRequestFactory rf = new SimpleClientHttpRequestFactory();
+
+		if(!StringUtils.isEmpty(readTimeout)) {
+			System.out.println("allo");
+			rf.setReadTimeout(Integer.valueOf(readTimeout) * 1000);
+		}
+
+		if(!StringUtils.isEmpty(connectTimeout)) {
+			rf.setConnectTimeout(Integer.valueOf(connectTimeout) * 1000);
+		}
+
+		return rf;
+	}
+
+	@Bean
+	public RestTemplate restTemplate(ClientHttpRequestFactory requestFactory) {
+		RestTemplate rt = new RestTemplate(requestFactory);
 
 		return rt;
 	}
