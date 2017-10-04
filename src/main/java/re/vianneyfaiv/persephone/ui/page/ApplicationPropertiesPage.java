7331c13376cdabfeddd723363a71db4105946f08
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-10-03 22:46:47 -0400

    implemented filtering by property key

--- src/main/java/re/vianneyfaiv/persephone/ui/page/ApplicationPropertiesPage.java
+++ src/main/java/re/vianneyfaiv/persephone/ui/page/ApplicationPropertiesPage.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import re.vianneyfaiv.persephone.domain.Application;
@@ -32,6 +35,9 @@ public class ApplicationPropertiesPage extends VerticalLayout implements View {
 	@Autowired
 	private EnvironmentService envService;
 
+	private Environment currentEnv;
+	private Grid<PropertyItem> propertiesGrid;
+
 	@PostConstruct
 	public void init() {
 	}
@@ -48,17 +54,39 @@ public class ApplicationPropertiesPage extends VerticalLayout implements View {
 			// TODO throw exception
 		}
 
+		// Load properties
+		this.currentEnv = envService.getEnvironment(app.get());
+
 		// Properties grid
-		Environment env = envService.getEnvironment(app.get());
-		Grid<PropertyItem> grid = new Grid<>(PropertyItem.class);
-		grid.setColumns("key", "value", "origin");
-		grid.setItems(env.getProperties());
-		grid.sort("key");
-		grid.setSizeFull();
+		this.propertiesGrid = new Grid<>(PropertyItem.class);
+		this.propertiesGrid.setColumns("key", "value", "origin");
+		this.propertiesGrid.sort("key");
+		this.propertiesGrid.setSizeFull();
+
+		// Filter by Property
+		TextField filterInput = new TextField();
+		filterInput.setPlaceholder("filter by property key...");
+		filterInput.addValueChangeListener(e -> updateProperties(e.getValue()));
+		filterInput.setValueChangeMode(ValueChangeMode.LAZY);
 
 		this.addComponent(new PageTitle(app.get(), "Properties"));
-		this.addComponent(new ButtonBar());
-		this.addComponent(grid);
+		this.addComponent(new ButtonBar(filterInput));
+		this.addComponent(this.propertiesGrid);
+		this.updateProperties("");
+	}
+
+	private void updateProperties(String filterByPropKey) {
+
+		if(StringUtils.isEmpty(filterByPropKey)) {
+			this.propertiesGrid.setItems(this.currentEnv.getProperties());
+		}
+		else {
+			this.propertiesGrid.setItems(
+				this.currentEnv.getProperties()
+					.stream()
+					.filter(p -> p.getKey().contains(filterByPropKey))
+			);
+		}
 	}
 
 }
