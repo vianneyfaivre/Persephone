8f6fc104a215dcbd40df821325674fed44089cc6
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-11-08 19:24:29 -0500

    Consider applications are using Actuator v1 by default

--- src/main/java/re/vianneyfaiv/persephone/domain/env/ActuatorVersion.java
+++ src/main/java/re/vianneyfaiv/persephone/domain/env/ActuatorVersion.java
@@ -5,20 +5,22 @@ import org.springframework.http.MediaType;
 
 public enum ActuatorVersion {
 
-	V1(ActuatorMediaTypes.APPLICATION_ACTUATOR_V1_JSON),
+	V1(ActuatorMediaTypes.APPLICATION_ACTUATOR_V1_JSON_VALUE),
+	V2("application/vnd.spring-boot.actuator.v2+json"),
 	NOT_SUPPORTED(null);
 
-	private MediaType version;
+	private String mediaType;
 
-	private ActuatorVersion(MediaType version) {
-		this.version = version;
+	private ActuatorVersion(String mediaType) {
+		this.mediaType = mediaType;
 	}
 
 	public static ActuatorVersion parse(MediaType contentType) {
-		if(contentType.getSubtype().equals(V1.version.getSubtype())) {
-			return V1;
+		if(V2.mediaType.equals(contentType.getSubtype())) {
+			return NOT_SUPPORTED;
 		}
-		return NOT_SUPPORTED;
+		// By default, consider it's Actuator from spring boot 1.x
+		return V1;
 	}
 
 }
