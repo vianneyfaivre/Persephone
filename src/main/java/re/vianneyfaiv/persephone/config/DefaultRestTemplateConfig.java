b64806940f676cfcd99eb7e7455d5ce32d9254d0
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-11-08 19:56:03 -0500

    set default Accept http header value

--- src/main/java/re/vianneyfaiv/persephone/config/DefaultRestTemplateConfig.java
+++ src/main/java/re/vianneyfaiv/persephone/config/DefaultRestTemplateConfig.java
@@ -1,12 +1,20 @@
 package re.vianneyfaiv.persephone.config;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
@@ -54,6 +62,20 @@ public class DefaultRestTemplateConfig {
 		// Override default error handler to consider HTTP 3xx 4xx and 5xx as errors
 		rt.setErrorHandler(restErrorHandler);
 
+		// Default HTTP 'Accept' header value will be application/json
+		rt.getInterceptors().add(new AcceptJsonRequestInterceptor());
+
 		return rt;
+	}
+
+	static class AcceptJsonRequestInterceptor implements ClientHttpRequestInterceptor {
+
+		@Override
+		public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+				throws IOException {
+			request.getHeaders().setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+			return execution.execute(request, body);
+		}
+
 	}
 }
