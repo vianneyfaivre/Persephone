38ece7ff0c64ece6824935fde6d81b7ea67158eb
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-11-20 18:19:38 -0500

    generate application.pid file

--- src/main/java/re/vianneyfaiv/PersephoneApplication.java
+++ src/main/java/re/vianneyfaiv/PersephoneApplication.java
@@ -3,6 +3,7 @@ package re.vianneyfaiv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude= {
@@ -12,6 +13,10 @@ import org.springframework.context.annotation.PropertySource;
 public class PersephoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersephoneApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(PersephoneApplication.class);
+
+		springApplication.addListeners(new ApplicationPidFileWriter());
+
+		springApplication.run(args);
 	}
 }
